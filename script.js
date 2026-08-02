5e5d235ff5d608d2ef505092c6de7fb891ce1a0b
jsembestie55-a11y <jsembestie55@gmail.com>
2026-08-03 01:40:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const players = [
-    {name: "Kylian Mbappé", rating: 91, rarity: "Gold"},
-    {name: "Erling Haaland", rating: 91, rarity: "Gold"},
-    {name: "Lionel Messi", rating: 88, rarity: "Legend"},
-    {name: "Cristiano Ronaldo", rating: 86, rarity: "Legend"},
-    {name: "Jude Bellingham", rating: 90, rarity: "Gold"},
-    {name: "Vinícius Jr.", rating: 89, rarity: "Gold"}
+    {name: "Kylian Mbappé", rating: 91, position: "ST", rarity: "Gold"},
+    {name: "Erling Haaland", rating: 91, position: "ST", rarity: "Gold"},
+    {name: "Lionel Messi", rating: 88, position: "RW", rarity: "Legend"},
+    {name: "Cristiano Ronaldo", rating: 86, position: "ST", rarity: "Legend"},
+    {name: "Jude Bellingham", rating: 90, position: "CM", rarity: "Gold"},
+    {name: "Vinícius Jr.", rating: 89, position: "LW", rarity: "Gold"}
 ];
 
 let coins = 1000;
@@ -23,15 +23,20 @@ function openPack(){
     let player = players[random];
 
     document.getElementById("card").innerHTML = `
-        <div style="
-        background:#222;
-        padding:20px;
-        border-radius:15px;">
-        
-        <h2>${player.name}</h2>
+    
+    <div class="card">
+
         <h1>${player.rating}</h1>
+
+        <div class="position">
+        ${player.position}
+        </div>
+
+        <h2>${player.name}</h2>
+
         <p>${player.rarity}</p>
 
-        </div>
+    </div>
+
     `;
 }
